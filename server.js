500fd310911d8f59c3bbf78c9103f110854d68b8
Libikk <libikpl@gmail.com>
2019-06-11 18:53:55 +0100

    node server huraaaa - easy

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const next = require('next');
+
+const dev = process.env.NODE_ENV === 'development';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+
+nextApp.prepare()
+  .then(() => {
+
+    app.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+    });
+})
