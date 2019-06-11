fcccb1ea503cbaa502e78ff864b65841fe197a13
Libikk <libikpl@gmail.com>
2019-06-11 18:39:00 +0100

    initial project next, type script, react

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import '../styles/styles.scss'
+
+import React from 'react';
+export default class extends React.Component {
+  render() {
+    return (
+      <div className="title">
+        Hello Next.js
+      </div>
+    )
+  }
+}
