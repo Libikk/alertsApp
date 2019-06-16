e1fdb4c04ce65ce962285dba8b229c3ecbc3e62c
Libikk <libikpl@gmail.com>
2019-06-16 18:29:21 +0100

    seed config col;umn name change

--- sql/dataBaseSeed/seedConfig.js
+++ sql/dataBaseSeed/seedConfig.js
@@ -22,32 +22,32 @@ const seedConfig = {
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"509-barley-grass-ground-dried-grass-100g-intenson.html"'
+            productUrl: '"509-barley-grass-ground-dried-grass-100g-intenson.html"'
         },
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"wheat-seeds-rice/1101-energy-mix-nuts-dobra-kaloria-200g.html"'
+            productUrl: '"wheat-seeds-rice/1101-energy-mix-nuts-dobra-kaloria-200g.html"'
         },
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"63-vanilla-pudding-organic-gluten-free-40g-amylon-8594006666473.html"'
+            productUrl: '"63-vanilla-pudding-organic-gluten-free-40g-amylon-8594006666473.html"'
         },
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"117-tahini-sesame-butter-185g-primavika-5900672300871.html"'
+            productUrl: '"117-tahini-sesame-butter-185g-primavika-5900672300871.html"'
         },
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"natural-cosmetics/932-deo-sport-xufas-in-cream.html"'
+            productUrl: '"natural-cosmetics/932-deo-sport-xufas-in-cream.html"'
         },
         {
             websiteId: 1,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
-            url: '"932-deo-sport-xufas-in-cream.html"'
+            productUrl: '"932-deo-sport-xufas-in-cream.html"'
         },
 
 
@@ -55,49 +55,49 @@ const seedConfig = {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837"' // yes
+            productUrl: '"ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837"' // yes
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"nicotine-gums/asda-nicotine-replace-2mg-gum-peppermint-flavour/910002911935"' // no
+            productUrl: '"nicotine-gums/asda-nicotine-replace-2mg-gum-peppermint-flavour/910002911935"' // no
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"fillets-grills/birds-eye-2-chicken-chargrills-periperi/910003005798"' // no
+            productUrl: '"fillets-grills/birds-eye-2-chicken-chargrills-periperi/910003005798"' // no
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"kids-yogurts/yeo-valley-little-yeos-fruity-favourites-yogurt/1000000471824"'// no
+            productUrl: '"kids-yogurts/yeo-valley-little-yeos-fruity-favourites-yogurt/1000000471824"'// no
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"chicken-turkey/asda-4-lime-coriander-chicken-breast-sizzle-steaks/1000132169293"'// no
+            productUrl: '"chicken-turkey/asda-4-lime-coriander-chicken-breast-sizzle-steaks/1000132169293"'// no
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"kids-yogurts/frubes-strawberry-flavour-yogurt/1000078555632"' //yes
+            productUrl: '"kids-yogurts/frubes-strawberry-flavour-yogurt/1000078555632"' //yes
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"oil/ktc-pure-butter-ghee/27535622"' // yes
+            productUrl: '"oil/ktc-pure-butter-ghee/27535622"' // yes
         },
         {
             websiteId: 2,
             createdAt: `"${moment().format('YYYY-MM-DD hh:ss')}"`,
             isClientSideCheck: true,
-            url: '"cooking-oils/east-end-pure-butter-ghee-for-cooking/910002823480"' // no
+            productUrl: '"cooking-oils/east-end-pure-butter-ghee-for-cooking/910002823480"' // no
         }
     ]
 }
