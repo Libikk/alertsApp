052ea8c0a0c6657ab19f677080f148117c3b3ad9
Libikk <libikpl@gmail.com>
2019-06-16 18:27:56 +0100

    sql server

--- sql/sqlServer.js
+++ sql/sqlServer.js
@@ -1,14 +1,8 @@
-connection = require('mysql').createConnection({
+const sql = require('mysql').createConnection({
     host: 'localhost',
     user: 'sytek',
     password: 'Cycki12345',
     database: 'discouthero'
 });
 
-connection.connect((err) => {
-if (err) throw err;
-console.log('Connected to SQL!');
-});
-
-
-module.exports = connection
+module.exports = sql;
