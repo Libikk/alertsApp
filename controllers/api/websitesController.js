6a261d677c2e293a6cec009491d8b8cca64042f2
Libikk <libikpl@gmail.com>
2019-06-19 05:46:30 +0100

    change to es6 fun

--- controllers/api/websitesController.js
+++ controllers/api/websitesController.js
@@ -1,7 +1,7 @@
 const sql = require('../../sql/sqlServer');
 const websites = {
     getWebsitesWithProducts: () => {
-        return new Promise(function(resolve, reject) {
+        return new Promise((resolve, reject) => {
             var sqlQuery = require('fs').readFileSync('./controllers/sqlQueries/getDataForScan.sql').toString();
             sql.query(sqlQuery, (err, result, fields) => {
                 if (err) return reject(err);
