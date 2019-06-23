3720442032ae68bfc3ffb464b2fdad1e709f7fd3
Libikk <libikpl@gmail.com>
2019-06-23 08:14:42 +0100

    add timeout, close browser once finished, default to not promo if there is no selector found

--- productScaner/ClientSide.js
+++ productScaner/ClientSide.js
@@ -1,14 +1,26 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const executeCheck = async (link, selector, page, options) => {
-    await page.goto(link)// || 'https://groceries.asda.com/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837');
-    await page.waitForSelector(selector);
+const opt = {
+    timeout: 6000
+}
 
-    const text = await page.$eval(selector, (el) => el.textContent);
-    var a = (new RegExp(options.regexCheck, 'ig')).test(text)
-    console.log('isPromo clientSide: ', a, link)
-    return a;
+const executeCheck = async (link, selector, page, options) => {
+    const result = {
+        isPromo: false
+    }
+    try {
+        await page.goto(link)// || 'https://groceries.asda.com/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837');
+        await page.waitForSelector(selector, opt);
+
+        const text = await page.$eval(selector, (el) => el.textContent);
+        result.isPromo = (new RegExp(options.regexCheck, 'ig')).test(text)
+        console.log('isPromo clientSide: ', result.isPromo, link)
+    } catch(err) {
+        console.error('err: ',link ,err);
+        result.isError = true
+    }
+    return result;
 }
 
 const getClientSideCheck = async (listOfProducts) => {
@@ -18,11 +30,11 @@ const getClientSideCheck = async (listOfProducts) => {
         const page = await browser.newPage();
         for (const singleProduct of listOfProducts) {
             if (singleProduct.isClientSideCheck) {
-                var isPromo = await executeCheck(singleProduct.fullUrl, singleProduct.selectorString, page, { regexCheck: singleProduct.regex })
-                prod.push(Object.assign({}, singleProduct, { isPromo }))
+                const checkResult = await executeCheck(singleProduct.fullUrl, singleProduct.selectorString, page, { regexCheck: singleProduct.regex })
+                prod.push(Object.assign({}, singleProduct, checkResult))
             }
-
         };
+        browser.close()
     } catch( err){
         console.error(err)
     }
