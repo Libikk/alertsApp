ed24e8e793285bfe1ae99bf12cbc3a66270623be
Libikk <libikpl@gmail.com>
2019-06-23 10:31:47 +0100

    schedule job 10 am every day

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const next = require('next');
+const moment = require('moment')
 
 const { sql, sqlQuery } = require('./sql/sqlServer');
 const dev = process.env.NODE_ENV === 'development';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
+const scan = require('./productScaner/scan');
+
+var schedule = require('node-schedule');
+var plannedJob = schedule.scheduleJob({ hour: 10, minute: 28 }, () => scan()); // every day, 10am
 
 
 nextApp.prepare()
