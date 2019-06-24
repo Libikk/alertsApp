73d790fc954eaeed87b422bcafff055d6c7f6a8a
Libikk <libikpl@gmail.com>
2019-06-24 19:05:34 +0100

    lint all and disable few rules

--- productScaner/ServerSide.js
+++ productScaner/ServerSide.js
@@ -2,25 +2,23 @@ const request = require('request');
 const cheerio = require('cheerio');
 const _ = require('lodash');
 
-getServerSideCheck = (listOfProducts) => {
-    const filteredProducts = _.cloneDeep(listOfProducts).filter(e => !e.isClientSideCheck);
-    return filteredProducts.map(singleProduct => {
-        return new Promise((resolve, reject) => {
-            request(singleProduct.fullUrl, (error, response, body) => {
-                // to do error handle
-                const $ = cheerio.load(body)
-                const disc = $(listOfProducts.selector)
-                if (singleProduct.regex) {
-                    const isPromo = (new RegExp(listOfProducts.regex, 'gi')).test(disc.text())
-                    console.log('isPromo serverSide: ', isPromo, '  ', singleProduct.fullUrl)
-                    resolve(Object.assign({}, singleProduct, { isPromo }))
-                }
-            });
-        });
+const getServerSideCheck = (listOfProducts) => {
+  const filteredProducts = _.cloneDeep(listOfProducts).filter(e => !e.isClientSideCheck);
+  // eslint-disable-next-line no-unused-vars
+  return filteredProducts.map(singleProduct => new Promise((resolve, reject) => {
+    request(singleProduct.fullUrl, (error, response, body) => {
+      // to do error handle
+      const $ = cheerio.load(body);
+      const disc = $(listOfProducts.selector);
+      if (singleProduct.regex) {
+        const isPromo = (new RegExp(listOfProducts.regex, 'gi')).test(disc.text());
+        console.log('isPromo serverSide: ', isPromo, '  ', singleProduct.fullUrl);
+        resolve(Object.assign({}, singleProduct, { isPromo }));
+      }
     });
-
-}
+  }));
+};
 
 module.exports = {
-    getServerSideCheck
-}
+  getServerSideCheck,
+};
--- productScaner/scan.js
+++ productScaner/scan.js
@@ -1,30 +1,23 @@
-const serverSideScan = require('./ServerSide');
+const serverSideScan = require('./ServerSide').default;
 const clientSideScan = require('./ClientSide');
 const websitesController = require('../controllers/api/websitesController');
-const { sql, sqlQuery } = require('../sql/sqlServer');
+const { sqlQuery } = require('../sql/sqlServer');
 const _ = require('lodash');
 
-saveSingleItem = () => {
-
-
-}
-
-scanProducts = () => {
-    websitesController.getWebsitesWithProducts()
-        .then((allProductsWithWebsites) => {
-            const webScan = clientSideScan.getClientSideCheck(allProductsWithWebsites)
-            const serverScan = serverSideScan.getServerSideCheck(allProductsWithWebsites)
-            Promise.all(_.concat([], webScan, serverScan)).then(res =>{
-                const flattenedResp = _.flattenDeep(res)
-                flattenedResp.forEach(single => {
-                    const params = [single.productId, single.isPromo, single.isError]
-                    const sql = `INSERT INTO scans (productId, createdAt, isPromo, isError) values (?, now(), ?, ?)`
-                    sqlQuery(sql, params)
-                    //to do update all of them in one query
-                });
-              }
-            )
-        })
-
-}
-module.exports =  scanProducts
+const scanProducts = () => {
+  websitesController.getWebsitesWithProducts()
+    .then((allProductsWithWebsites) => {
+      const webScan = clientSideScan.getClientSideCheck(allProductsWithWebsites);
+      const serverScan = serverSideScan.getServerSideCheck(allProductsWithWebsites);
+      Promise.all(_.concat([], webScan, serverScan)).then((res) => {
+        const flattenedResp = _.flattenDeep(res);
+        flattenedResp.forEach((single) => {
+          const params = [single.productId, single.isPromo, single.isError];
+          const sqlString = 'INSERT INTO scans (productId, createdAt, isPromo, isError) values (?, now(), ?, ?)';
+          sqlQuery(sqlString, params);
+          // to do update all of them in one query
+        });
+      });
+    });
+};
+module.exports = scanProducts;
