a68adac42b7ce68d03e3b7b524b5d439317b9cfa
Libikk <libikpl@gmail.com>
2019-06-26 05:25:12 +0100

    dev server fix, lint fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@ import '../styles/styles.scss'
 
 import React from 'react';
 export default class extends React.Component {
-  test = (a: String) => {
-    return a + 'dsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'
+  state = {
+    a: 100
   }
+  test = (a: Number) => {
+    this.setState({ a: 'huasdasdasdadsugsdfj' + a })
+   }
 
   render() {
     return (
       <div className="title">
-        Hello Next.js this is testtt {this.test(true)}
+        Hello Next.js this is testtt {this.state.a}
+        <button onClick={() => this.test(213)}></button>
       </div>
     )
   }
