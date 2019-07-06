aba1bfab30b46897d445cee6afc0903a35b1cfe6
Libikk <libikpl@gmail.com>
2019-07-06 12:50:04 +0100

    store provider plus simple test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import '../styles/styles.scss'
+import { getWebsitesWithProducts }from '../dispatchers/websitesDispatchers';
+
 import Layout from '../components/Layout';
 
-import React from 'react';
-export default class extends React.Component {
-  state = {
-    a: 100
-  }
-  test = (a: Number) => {
-    this.setState({ a: 'huasdasdasdadsugsdfj' + a })
+
+class Index extends React.Component {
+  test = () => {
+    this.props.getWebsitesWithProducts();
    }
 
   render() {
+    const { websites } = this.props;
     return (
       <Layout>
         <div className="title">
           DISCOUT HERO
-          Hello Next.js this is testtt {this.state.a}
-          <button onClick={() => this.test(213)}></button>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <button onClick={() => this.test()}>ttttest</button>
+          <button onClick={() => console.log(this.state, this.props)}>state</button>
+          {
+            websites.websitesList ? websites.websitesList.map(singleWebsite => <div key={singleWebsite.websiteId}>{singleWebsite.createdAt}   ghagaga {singleWebsite.url}</div>) : null
+          }
         </div>
       </Layout>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  getWebsitesWithProducts: bindActionCreators(getWebsitesWithProducts, dispatch),
+});
+
+export default connect(state => state, mapDispatchToProps)(Index);
