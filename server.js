a7c5c81c156112eb0bc2b07b37729d80dc04238f
Libikk <libikpl@gmail.com>
2019-07-13 16:49:43 +0100

    body parser + auth controller

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const next = require('next');
+const bodyParser = require('body-parser');
 // eslint-disable-next-line no-unused-vars
 const { sql, sqlQuery } = require('./sql/sqlServer');
 
@@ -12,14 +13,18 @@ const schedule = require('node-schedule');
 
 const websitesController = require('./controllers/api/websitesController2');
 const scansController = require('./controllers/api/scansController');
+const authController = require('./controllers/api/authController');
 
 schedule.scheduleJob({ hour: 10, minute: 28 }, () => scan()); // every day, 10am
 
 
 nextApp.prepare()
   .then(() => {
+    app.use(bodyParser.json());
+
     app.use('/api/websites', websitesController);
     app.use('/api/scans', scansController);
+    app.use('/api/auth', authController);
 
     app.get('/test', (req, res) => {
       nextApp.render(req, res, '/test');
@@ -29,6 +34,6 @@ nextApp.prepare()
 
     server.listen(3000, (err) => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+
     });
   });
