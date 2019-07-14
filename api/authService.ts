02d2e8f7b6dcb76f35001e7461bcd479e8866b2c
Libikk <libikpl@gmail.com>
2019-07-14 17:47:21 +0100

    auth service

--- api/authService.ts
+++ api/authService.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export default class AuthService {
   static register = async (data) => axios.post('http://localhost:3000/api/auth/register', data).then(response => response);
 
-  static login = async () => axios.post('http://localhost:3000/api/auth/login').then(response => response);
+  static login = async ({ email, password }) => axios.post('http://localhost:3000/api/auth/login', { email, password }).then(response => response);
 }
