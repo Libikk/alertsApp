95cff66b190975c843924b1d6283f0791abbc49a
Libikk <libikpl@gmail.com>
2019-07-14 17:47:10 +0100

    add to the server cookieparser and test req

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const next = require('next');
 const bodyParser = require('body-parser');
 // eslint-disable-next-line no-unused-vars
 const { sql, sqlQuery } = require('./sql/sqlServer');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const dev = process.env.NODE_ENV !== 'development';
 const nextApp = next({ dev });
@@ -21,13 +23,20 @@ schedule.scheduleJob({ hour: 10, minute: 28 }, () => scan()); // every day, 10am
 nextApp.prepare()
   .then(() => {
     app.use(bodyParser.json());
+    app.use(cookieParser());
 
     app.use('/api/websites', websitesController);
     app.use('/api/scans', scansController);
     app.use('/api/auth', authController);
 
     app.get('/test', (req, res) => {
-      nextApp.render(req, res, '/test');
+      const token = req.cookies.access_token;
+      console.log('req.cookies: ', req.cookies);
+      console.log('token: ', token);
+      const secret = 'FdsfDSF1dsfD__2..SFDS34)_;L;';
+      const decoded = jwt.verify(token, secret);
+      console.log('decoded: ', decoded);
+      res.send(decoded)
     });
 
     app.get('*', (req, res) => handle(req, res));
