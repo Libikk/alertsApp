8ad83bd0b22644db22a361968520840547ba63c6
Libikk <libikpl@gmail.com>
2019-07-18 07:15:14 +0100

    lint and catch errors

--- controllers/api/authController.js
+++ controllers/api/authController.js
@@ -3,32 +3,28 @@ const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const { sqlQuery } = require('../../sql/sqlServer');
-const getQuery = require('../getQuery');
 
 const register = async (req, res, next) => {
   const { userName, email, password } = req.body;
   if (userName && email && password) {
     const hashPass = await bcrypt.hash(password, 10);
-
     sqlQuery('insert into users (userName, email, password, createdAt) values (?, ?, ?, ?)', [userName, email, hashPass, new Date()])
-      .then((e) => {
-        res.send(200).json(res.body);
-      });
+      .then(() => res.sendStatus(200))
+      .catch(next);
   }
 };
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   if (email && password) {
-    const user = await sqlQuery('select * from users where email = ?', [email]).then(e => e[0]);
-
+    const user = await sqlQuery('select * from users where email = ?', [email]).then(e => e[0]).catch(next);
     if (!user) {
       return next(new Error('Wrong Email or Password'));
     }
 
     if (bcrypt.compareSync(password, user.password)) {
       sqlQuery('UPDATE users set lastLoggedIn = now() where email = ?', [email]);
-      return res.send({ userName: user.userName, userId: user.userId, email: user.email, lastLoggedIn: new Date() });
+      return res.send({ userName: user.userName, userId: user.userId, email: user.email, lastLoggedIn: new Date() }).catch(next);
     }
     return next(new Error('Wrong Password'));
   }
