3bf439dd11858331306038df81433c89ba688604
Libikk <libikpl@gmail.com>
2019-07-26 19:41:18 +0100

    autorize service

--- api/authService.ts
+++ api/authService.ts
@@ -4,4 +4,6 @@ export default class AuthService {
   static register = async (data) => axios.post('http://localhost:3000/api/auth/register', data).then(({ data }) => data);
 
   static login = async ({ email, password }) => axios.post('http://localhost:3000/api/auth/login', { email, password }).then(({ data }) => data);
+
+  static authorize = async (token) => axios.post('http://localhost:3000/api/auth/authorize', { token }).then(({ data }) => data);
 }
