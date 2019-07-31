5d879eed9fdc31f5a5aa6abc2fe83901b85447d3
Libikk <libikpl@gmail.com>
2019-07-31 06:32:59 +0100

    sql change according to seed change

--- controllers/sqlQueries/getDataForScan.sql
+++ controllers/sqlQueries/getDataForScan.sql
@@ -1,7 +1,7 @@
 SELECT
     productId,
     web.websiteId,
-    CONCAT(web.urlToProduct, prod.productUrl) as "fullUrl",
+    prod.productUrl as "fullUrl",
     IFNULL(prod.regexCheck, web.regexCheck) as "regex",
     IFNULL(prod.selector, web.selector) as "selectorString",
     (case IFNULL(prod.isClientSideCheck, web.isClientSideCheck) when '0' then 0 when '1' then 1 end) as "isClientSideCheck"
