beed8abcbf0e757b9e52e8fd29a8255289e0a3de
Libikk <libikpl@gmail.com>
2019-08-12 19:53:11 +0100

    product save create website product id params

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const url = require('url');
+const passport = require('../../passportStrategy');
 
 const router = express.Router();
 const { sqlQuery } = require('../../sql/sqlServer');
@@ -11,6 +12,27 @@ const checkProdExistence = (req, res, next) => {
     .catch(next);
 };
 
+
+const addUserProduct = async (req, res, next) => {
+// VALIDATE PAYLOAD
+  let productId = req.body.productId;
+
+  if (req.body.productUrl) {
+    const parsedUrl = url.parse(req.body.productUrl);
+    const newWebsiteParams = [parsedUrl.host, `${parsedUrl.protocol}//${parsedUrl.host}`, 0, parsedUrl.host];
+    const newProductParams = [parsedUrl.href, parsedUrl.host];
+
+
+    await sqlQuery(getQuery('createNewWebsite'), newWebsiteParams);
+    const newProduct = await sqlQuery(getQuery('createNewProduct'), newProductParams);
+    productId = newProduct.insertId;
+  }
+
+  // ADD PRODUCT TO USER LIST
+  res.send([req.params, req.body, req.query, productId]);
+};
+
 router.get('/productExistence', checkProdExistence);
+router.post('/addUserProduct', passport.authenticate('jwt', { session: false }), addUserProduct);
 
 module.exports = router;
