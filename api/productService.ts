f979bbebb4b4a8116ff3eb194c452d27c0f05864
Libikk <libikpl@gmail.com>
2019-08-14 06:20:00 +0100

    addUserProduct

--- api/productService.ts
+++ api/productService.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export class ProductService {
   static checkProdExistence = async (productUrlData) => axios.get(`http://localhost:3000/api/product/productExistence`, { params: { productUrlData } }).then(({ data }) => data);
+
+  static addUserProduct = async (data) => axios.post(`http://localhost:3000/api/product/addUserProduct`, data).then(({ data }) => data);
+
 }
--- dispatchers/productDispatchers.ts
+++ dispatchers/productDispatchers.ts
@@ -5,5 +5,9 @@ import { ProductService } from '../api/productService';
 export const checkProdExistence = (productUrlData) => (dispatch) => {
   return ProductService.checkProdExistence(productUrlData)
     .then(res => dispatch({ type: 'CHECK_PRODUCT_EXISTENCE', prodData: res })
-);
-};
+)};
+
+export const addUserProduct = (productUrlData) => (dispatch) => {
+  return ProductService.addUserProduct(productUrlData)
+    .then(res => dispatch({ type: 'ADD_USER_PRODUCT', prodData: res })
+ )};
