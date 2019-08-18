98da8babc64bc99c9c71f645285473fbf2876e7f
Libikk <libikpl@gmail.com>
2019-08-18 15:46:56 +0100

    more info about user products

--- controllers/sqlQueries/getUserProducts.sql
+++ controllers/sqlQueries/getUserProducts.sql
@@ -1,9 +1,11 @@
 SELECT
-	usp.createdAt AS "addedAt",
+	  usp.createdAt AS "addedAt",
     prod.productUrl,
     prod.productId,
+    web.hostName,
+    web.url AS "webUrl",
     case prod.isActive when '0' then 0 when '1' then 1 end AS "isProductActive",
-    scan.isPromo
+    case scan.isPromo when '0' then 0 when '1' then 1 end AS "isPromo"
 FROM discounthero.products prod
 JOIN discounthero.usersproducts usp ON usp.userId = ?
 	AND usp.deletedAt IS NULL
