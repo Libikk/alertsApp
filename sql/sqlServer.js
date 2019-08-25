2eb543e5416aa51f80bb045e3428927dbfaaa791
Libikk <libikpl@gmail.com>
2019-08-25 16:33:51 +0100

    make it objects instead keeping in seperated variables

--- sql/sqlServer.js
+++ sql/sqlServer.js
@@ -7,25 +7,25 @@ const sql = require('mysql').createConnection({
   database: 'discounthero',
 });
 
-const executeRawSQL = (query, params) => new Promise((resolve, reject) => {
-  sql.query(query, params, (error, response) => {
-    if (error) reject(error);
-    resolve(response);
-  });
-});
-
-const sqlQuery = (queryNameOrRaw, params) => {
-  const rawSql = getQuery(queryNameOrRaw) || queryNameOrRaw;
-
-  if (_.isObject(params) && !_.isArray(params)) {
+const mysqlQueries = {
+  executeRawSQL: (query, params) => new Promise((resolve, reject) => {
+    sql.query(query, params, (error, response) => {
+      if (error) reject(error);
+      resolve(response);
+    });
+  }),
+  sqlQuery: (queryNameOrRaw, params) => {
+    const rawSql = getQuery(queryNameOrRaw) || queryNameOrRaw;
+    if (_.isObject(params) && !_.isArray(params)) return mysqlQueries.replaceVariablesInQuery(params, rawSql);
+    return mysqlQueries.executeRawSQL(rawSql, params);
+  },
+  replaceVariablesInQuery: (params, rawSql) => {
     const replacedParamsSql = Object.keys(params).reduce((query, nextParamKey) => {
       const singleParamValue = sql.escape(params[nextParamKey]);
       return query.replace(new RegExp(nextParamKey, 'g'), singleParamValue);
     }, rawSql);
-    return executeRawSQL(replacedParamsSql);
-  }
-
-  return executeRawSQL(rawSql, params);
+    return mysqlQueries.executeRawSQL(replacedParamsSql);
+  },
 };
 
-module.exports = { sql, sqlQuery, executeRawSQL };
+module.exports = { sql, ...mysqlQueries };
