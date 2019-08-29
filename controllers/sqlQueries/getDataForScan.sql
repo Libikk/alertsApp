2de14dcf2c4ebe084ca29a57027f7788ed915d8c
Libikk <libikpl@gmail.com>
2019-08-29 18:25:45 +0100

    sql update product after scan and add more information to data for scan

--- controllers/sqlQueries/getDataForScan.sql
+++ controllers/sqlQueries/getDataForScan.sql
@@ -4,7 +4,9 @@ SELECT
     prod.productUrl as "fullUrl",
     IFNULL(prod.regexCheck, web.regexCheck) AS "regex",
     IFNULL(prod.selector, web.selector) AS "selectorString",
-    (case IFNULL(prod.isClientSideCheck, web.isClientSideCheck) WHEN '0' THEN 0 WHEN '1' THEN 1 END) AS "isClientSideCheck"
+    (case IFNULL(prod.isClientSideCheck, web.isClientSideCheck) WHEN '0' THEN 0 WHEN '1' THEN 1 END) AS "isClientSideCheck",
+    web.imageSelector,
+    web.productNameSelector
 FROM discounthero.products prod
 JOIN discounthero.websites web on prod.websiteId = web.websiteId
 WHERE web.isActive = '1'
