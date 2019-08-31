d7f3505dd784013eafafe79048798c0d918c08ac
Libikk <libikpl@gmail.com>
2019-08-31 14:09:23 +0100

    revert stuff

--- appConfig.js
+++ appConfig.js
@@ -2,9 +2,9 @@ const env = (process.env.NODE_ENV || 'development').trim();
 
 const config = {
   env: (process.env.NODE_ENV || 'development').trim(),
-  version: '1.00v',
-  envUrl: !env === 'production' ? 'http://kregielmarek.com' : 'http://localhost:3000',
-  port: !env === 'production' ? 80 : 3000,
+  version: '1.00',
+  envUrl: env === 'production' ? 'http://kregielmarek.com' : 'http://localhost:3000',
+  port: env === 'production' ? 80 : 3000,
 };
 
 module.exports = config;
