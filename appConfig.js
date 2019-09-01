9c5448e2f963f2226ad9b6c5e2b6de0b2a1d9568
Libikk <libikpl@gmail.com>
2019-09-01 12:49:14 +0100

    server ip

--- appConfig.js
+++ appConfig.js
@@ -3,7 +3,7 @@ const env = (process.env.NODE_ENV || 'development').trim();
 const config = {
   env: (process.env.NODE_ENV || 'development').trim(),
   version: '1.00',
-  envUrl: env === 'production' ? 'http://kregielmarek.com' : 'http://localhost:3000',
+  envUrl: env === 'production' ? 'http://http://142.93.35.49' : 'http://localhost:3000',
   port: env === 'production' ? 80 : 3000,
 };
 
