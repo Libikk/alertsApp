9933ab6184a6d1830b23570e6b601a550263bcbd
Libikk <libikpl@gmail.com>
2019-09-02 07:07:28 +0100

    v bum,p

--- appConfig.js
+++ appConfig.js
@@ -2,7 +2,7 @@ const env = (process.env.NODE_ENV || 'development').trim();
 
 const config = {
   env: (process.env.NODE_ENV || 'development').trim(),
-  version: '1.02',
+  version: '1.03',
   envUrl: env === 'production' ? 'http://142.93.35.49' : 'http://localhost:3000',
   port: env === 'production' ? 80 : 3000,
 };
