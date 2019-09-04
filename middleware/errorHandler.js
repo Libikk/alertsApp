0bf009733a37b52e82cded4a7a67b1118c142fb8
Libikk <libikpl@gmail.com>
2019-09-04 06:15:30 +0100

    add more info to errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,17 +1,25 @@
 const Sentry = require('@sentry/node');
+const { version } = require('../appConfig');
 
 Sentry.init({
   dsn: `https://${process.env.SENTRY_PUBLIC_KEY}@sentry.io/${process.env.SENTRY_PROJECT_ID}`,
   environment: process.env.NODE_ENV,
+  release: version,
 });
 
 const errorHandler = (err, req, res, next) => {
   if (!err) {
     return next();
   }
-  Sentry.captureException(err, 'Uncaught Exception thrown');
-  console.error('Error:  ', err.message);
-  res.status(err.status || 500);
+  const errCode = err.status || 500;
+
+  if (errCode === 500 || errCode === 404) {
+    Sentry.captureException(err, { userData: req.user });
+    console.error('User Data', req.user);
+    console.error('Err message ', err.message);
+  }
+
+  res.status(errCode);
   return res.json({
     ...err,
     msg: err.message,
