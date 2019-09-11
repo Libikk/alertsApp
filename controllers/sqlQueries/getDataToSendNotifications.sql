e6382145581b197188d463d9eb17ac6d129fb13f
Libikk <libikpl@gmail.com>
2019-09-11 10:51:14 +0100

    user email add

--- controllers/sqlQueries/getDataToSendNotifications.sql
+++ controllers/sqlQueries/getDataToSendNotifications.sql
@@ -8,7 +8,8 @@ SELECT
   prod.productName,
   CASE userNotif.emailNotifications WHEN 0 THEN 0 WHEN 1 THEN 1 END AS "emailNotifications",
   CASE userNotif.mobileAppNotifications  WHEN 0 THEN 0 WHEN 1 THEN 1 END AS "mobileAppNotifications",
-  CASE userNotif.smsNotifications  WHEN 0 THEN 0 WHEN 1 THEN 1 END AS "smsNotifications"
+  CASE userNotif.smsNotifications  WHEN 0 THEN 0 WHEN 1 THEN 1 END AS "smsNotifications",
+  user.email
 FROM
   discounthero.products AS prod
   LEFT JOIN discounthero.scans scan ON scan.scanid = (
@@ -25,6 +26,7 @@ FROM
   JOIN discounthero.websites website on website.websiteId = prod.websiteId
   JOIN discounthero.usersProducts userProd on userProd.productId = prod.productId
   JOIN discounthero.usersNotifications userNotif on userNotif.userId = userProd.userId
+  JOIN discounthero.users user on user.userid = userProd.userId
 WHERE
   scan.ispromo
   AND website.isActive = 1
