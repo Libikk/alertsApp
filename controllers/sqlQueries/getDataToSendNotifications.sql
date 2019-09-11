422a90992b469c7ee6407161cf719a9357055b95
Libikk <libikpl@gmail.com>
2019-09-11 10:06:56 +0100

    get datato send notifications -- all calculations in sql

---
+++ controllers/sqlQueries/getDataToSendNotifications.sql
@@ -0,0 +1,45 @@
+SELECT
+  prod.productId,
+  userProd.userId,
+  prod.productUrl,
+  website.hostName,
+  website.url as "hostNameUrl",
+  prod.imageUrl,
+  prod.productName,
+  userNotif.emailNotifications,
+  userNotif.mobileAppNotifications,
+  userNotif.smsNotifications
+FROM
+  discounthero.products AS prod
+  LEFT JOIN discounthero.scans scan ON scan.scanid = (
+    SELECT
+      scanLoop.scanid
+    FROM
+      discounthero.scans AS scanLoop
+    WHERE
+      scanLoop.productid = prod.productid
+    ORDER BY
+      scanLoop.createdat DESC
+    LIMIT 1
+  )
+  JOIN discounthero.websites website on website.websiteId = prod.websiteId
+  JOIN discounthero.usersProducts userProd on userProd.productId = prod.productId
+  JOIN discounthero.usersNotifications userNotif on userNotif.userId = userProd.userId
+WHERE
+  scan.ispromo
+  AND website.isActive = 1
+  AND prod.isActive = 1
+  AND DATEDIFF(NOW(),
+	 COALESCE((
+		SELECT
+			notifLoop.sentNotificationDate
+        FROM
+			notifications notifLoop
+        WHERE
+			notifLoop.userId = userProd.userId AND notifLoop.productId = prod.productId
+        ORDER BY
+			notifLoop.sentNotificationDate DESC
+		LIMIT 1
+  ), '1999-10-10')
+  ) > ?
+
