b544e5047c00711e4ae94be6a7d224e2230a535e
Libikk <libikpl@gmail.com>
2019-09-17 18:59:20 +0100

    load svg`s incline

--- components/howItWorks.tsx
+++ components/howItWorks.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../styles/howItWorks.scss';
-import SearchIcon from '@material-ui/icons/Search';
-import MoneyIcon from '@material-ui/icons/Money';
-import Icon from '@material-ui/core/Icon';
-import ReactSVG from 'react-svg'
+import FindIcon from '../static/svg/find.svg';
+import PiggyBankIcon from '../static/svg/piggybank.svg';
+import BellIcon from '../static/svg/notification-bell.svg';
 
 export default class HowItWorks extends React.Component {
 
@@ -11,30 +10,31 @@ export default class HowItWorks extends React.Component {
       const navConfig = [
         {
           headerText: 'FIND AND ADD PRODUCT TO YOUR ACCOUNT',
-          bodyText: '',
-          iconName: 'find'
+          bodyText: 'We work for the customer to make sure that shoppers get the best value for their household',
+          SvgIcon: FindIcon
         },
         {
           headerText: 'WAIT FOR DISCOUNT NOTIFICATION',
-          bodyText: '',
-          iconName: 'notification-bell'
+          bodyText: 'We work for the customer to make sure that shoppers get the best value for their household',
+          SvgIcon: BellIcon
         },
         {
-          headerText: 'SAVE MONEY',
-          bodyText: '',
-          iconName: 'piggybank'
+          headerText: 'SAVE ON AVERAGE 30% ON EACH SHOP!',
+          bodyText: 'Join the thousands of households in the UK that are shopping regularly with mySupermarket',
+          SvgIcon: PiggyBankIcon
         }
     ]
       return (
         <div className="container__howitworks">
             <h2>How does DDiscount Hero work?</h2>
             <ul className="howitworks__content">
-                {navConfig.map(({ headerText, bodyText, iconName }) => <li key={iconName}>
+                {navConfig.map(({ headerText, bodyText, SvgIcon }) => <li key={bodyText}>
                         <h2>{headerText}</h2>
-                        <ReactSVG className="content__icon" src={`../static/svg/${iconName}.svg`} />
-
+                        <p className="content__body-text">{bodyText}</p>
+                        <SvgIcon className="content__icon" />
                 </li>)}
             </ul>
+
         </div>
     );
   }
