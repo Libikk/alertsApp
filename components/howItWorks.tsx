752523106103b2aff850f25da8927df6e20209af
Libikk <libikpl@gmail.com>
2019-09-17 10:15:20 +0100

    add icons

--- components/howItWorks.tsx
+++ components/howItWorks.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import '../styles/howItWorks.scss';
 import SearchIcon from '@material-ui/icons/Search';
-
+import MoneyIcon from '@material-ui/icons/Money';
+import Icon from '@material-ui/core/Icon';
+import ReactSVG from 'react-svg'
 
 export default class HowItWorks extends React.Component {
 
     render() {
       const navConfig = [
         {
-          text: 'about us',
-          Icon: SearchIcon
+          headerText: 'FIND AND ADD PRODUCT TO YOUR ACCOUNT',
+          bodyText: '',
+          iconName: 'find'
         },
         {
-          text: 'support',
-          Icon: ''
+          headerText: 'WAIT FOR DISCOUNT NOTIFICATION',
+          bodyText: '',
+          iconName: 'notification-bell'
         },
         {
-          text: 'language',
-          Icon: ''
-        },
-        {
-          text: 'privacy',
-          Icon: ''
+          headerText: 'SAVE MONEY',
+          bodyText: '',
+          iconName: 'piggybank'
         }
     ]
       return (
         <div className="container__howitworks">
             <h2>How does DDiscount Hero work?</h2>
-            <ul className="howitworks__">
-                {navConfig.map(({text, Icon}) => <li>
-                        {text}
-                        {Icon && <Icon/>}
+            <ul className="howitworks__content">
+                {navConfig.map(({ headerText, bodyText, iconName }) => <li key={iconName}>
+                        <h2>{headerText}</h2>
+                        <ReactSVG className="content__icon" src={`../static/svg/${iconName}.svg`} />
 
                 </li>)}
             </ul>
