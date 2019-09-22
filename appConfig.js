2a9aacfdfd564f9759af6464965c30db8c3286e9
Libikk <libikpl@gmail.com>
2019-09-22 12:54:29 +0100

    v bump

--- appConfig.js
+++ appConfig.js
@@ -2,7 +2,7 @@ const env = (process.env.NODE_ENV || 'development').trim();
 
 const config = {
   env: (process.env.NODE_ENV || 'development').trim(),
-  version: '1.05',
+  version: '1.06',
   envUrl: env === 'production' ? 'http://142.93.35.49' : 'http://localhost:3000',
   port: env === 'production' ? 80 : 3000,
   sendProductNotificationsAgain: 15, // days
