34e8f858707771af81a27193b527d82e8e3a27ba
Libikk <libikpl@gmail.com>
2019-10-22 06:43:25 +0100

    auto login person on activation page

--- pages/activateAccount.tsx
+++ pages/activateAccount.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Layout from '../components/Layout';
+import { autorize } from '../dispatchers/authDispatchers';
+import { getCookie } from '../utils/auth';
+
 import { connect } from 'react-redux';
 
 type MyProps = {
@@ -12,7 +15,17 @@ type MyProps = {
 };
 
 class ActivateAccount extends React.Component<MyProps>  {
-  static getInitialProps = async ({ query }) => query.result;
+  static getInitialProps = async ({ req, query, isServer, store }) => {
+
+    if (isServer) {
+      const { cookie } = req.headers;
+      cookie && await autorize(getCookie('access_token', cookie))(store.dispatch)
+      // to do
+      // redirect to homepage or login page
+    }
+
+    return query.result
+  }
 
   render () {
     return (
