5167cd42b903b5e73777736f2a7c69aa82170275
Libikk <libikpl@gmail.com>
2019-11-16 12:32:32 +0000

    website service controller and sql query for data

--- api/websiteService.ts
+++ api/websiteService.ts
@@ -1,4 +1,6 @@
 import axiosInstance from '../axiosInstance';
 export class WebsiteService {
   static getWebsites = async () => axiosInstance.get('/api/websites/').then(response => response.data);
+
+  static getWebsitesSelectors = async () => axiosInstance.get('/api/websites/websitesSelectors').then(response => response.data);
 }
--- controllers/api/websitesController2.js
+++ controllers/api/websitesController2.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const { sqlQuery } = require('../../sql/sqlServer');
+const passport = require('../../passportStrategy');
+const { authenticateUser } = require('../../middleware/middleware');
 
 const getWebsites = (req, res, next) => {
   sqlQuery('SELECT * FROM discounthero.websites where isActive = 1')
@@ -9,6 +11,13 @@ const getWebsites = (req, res, next) => {
     .catch(next);
 };
 
+const websitesSelectors = (req, res, next) => {
+  sqlQuery('getWebsitesSelectors')
+    .then(response => res.send(response))
+    .catch(next);
+};
+
 router.get('/', getWebsites);
+router.get('/websitesSelectors', passport.authenticate('jwt', { session: false }), authenticateUser('admin'), websitesSelectors);
 
 module.exports = router;
