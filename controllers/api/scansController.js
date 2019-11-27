fc893dab9cfd92bea4c6c6a9558e11aa54befb2e
Libikk <libikpl@gmail.com>
2019-11-27 07:38:15 +0000

    scan controller test scan with security

--- controllers/api/scansController.js
+++ controllers/api/scansController.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 const { sqlQuery } = require('../../sql/sqlServer');
+const { testScan } = require('../../productScaner/scan');
+const passport = require('../../passportStrategy');
+const { authenticateUser } = require('../../middleware/middleware');
 
 const getCurrentdiscounts = (req, res, next) => {
   sqlQuery('currentDiscounts').then(response => res.send(response)).catch(next);
 };
 
 const testProductsScan = (req, res, next) => {
-  console.log(req.body)
+  testScan(req.body)
+    .then((response) => {
+      res.json(response);
+    })
+    .catch(next);
 }
 
 router.get('/currentDiscounts', getCurrentdiscounts);
-router.post('/testProductsScan', testProductsScan);
+router.post('/testProductsScan', passport.authenticate('jwt', { session: false }), authenticateUser('admin'), testProductsScan);
 
 module.exports = router;
