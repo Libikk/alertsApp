4eedaf43e37e58339f97335cbd82893ad9cb3719
Libikk <libikpl@gmail.com>
2019-11-27 06:59:58 +0000

    website and product managment table

--- components/Shared/WebsiteAndProductsManagement/WebsiteAndProductsManagement.tsx
+++ components/Shared/WebsiteAndProductsManagement/WebsiteAndProductsManagement.tsx
@@ -5,22 +5,13 @@ import { getWebsitesSelectors, updateWebsiteSelector } from '../../../dispatcher
 import { useDispatch, useSelector } from 'react-redux'
 import Modal from '@material-ui/core/Modal';
 import Card from '@material-ui/core/Card';
-import TextField from '@material-ui/core/TextField';
-import ProductTestScan from './ProductTestScan';
+import Button from '@material-ui/core/Button';
+import { testProductsScan } from '../../../dispatchers/scansDispatchers';
+import ProductTestedData from './ProductTestedData';
 import '../../../styles/websitesSelectorsManagement.scss'
-// // interface AuthObj {
-// //   currentUser: {
-// //     userName: string,
-// //     userId: number,
-// //     email: string
-// //   }
-// // }
 
-// type MyProps = {
-//   auth: AuthObj | null,
-// };
+
 const columns = [
-  { title: "hostName", field: "hostName" },
   { title: "url", field: "url" },
   { title: "isDiscountSelector", field: "isDiscountSelector" },
   { title: "isDiscountSelectorRegex", field: "isDiscountSelectorRegex" },
@@ -29,15 +20,20 @@ const columns = [
   { title: "productNameSelector", field: "productNameSelector" },
   { title: "productPriceSelector", field: "productPriceSelector" },
   { title: "productDiscountedPriceSelector", field: "productDiscountedPriceSelector" },
+  { title: "Discounted product url", field: "discountedProductUrl" },
+  { title: "NOT discounted product url", field: "notDiscountedProductUrl" },
 ]
 
+const testDataaa = [{"id":11,"isClientSideCheck":1,"isDiscountSelectorRegex":"rollback","isDiscountSelector":".pdp-main-details__promo-cntr","imageSelector":".s7staticimage img","productNameSelector":".pdp-main-details__title","productPriceSelector":".pdp-main-details__was-price","productDiscountedPriceSelector":".co-product__price.pdp-main-details__price","hostName":"groceries.asda.com","url":"https://groceries.asda.com","notDiscountedProductUrl":"https://groceries.asda.com/product/910000226861?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","discountedProductUrl":"https://groceries.asda.com/product/1000123650031?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","tableData":{"id":1,"checked":true},"fullUrl":"https://groceries.asda.com/product/1000123650031?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","regex":"rollback","selectorString":".pdp-main-details__promo-cntr","isPromo":true,"imgUrl":"https://ui.assets-asda.com/dm/asdagroceries/5057172103461_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=5yESd3&fmt=jpg&fit=constrain,1&wid=288&hei=288","productName":"ASDA Thick Sliced Oven Baked Dry Cured Ham","productPrice":"£4.50 /100g","productDiscountedPrice":"£4.00"},{"id":11,"isClientSideCheck":1,"isDiscountSelectorRegex":"rollback","isDiscountSelector":".pdp-main-details__promo-cntr","imageSelector":".s7staticimage img","productNameSelector":".pdp-main-details__title","productPriceSelector":".pdp-main-details__was-price","productDiscountedPriceSelector":".co-product__price.pdp-main-details__price","hostName":"groceries.asda.com","url":"https://groceries.asda.com","notDiscountedProductUrl":"https://groceries.asda.com/product/910000226861?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","discountedProductUrl":"https://groceries.asda.com/product/1000123650031?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","tableData":{"id":1,"checked":true},"fullUrl":"https://groceries.asda.com/product/910000226861?origin=/product/ham-pork-slices/asda-thick-dry-cured-ham-slices/910000226837","regex":"rollback","selectorString":".pdp-main-details__promo-cntr","isPromo":false,"imgUrl":"","productName":"ASDA Thick Sliced Breaded Dry Cured Ham Slices","productDiscountedPrice":"£3.00"}]
 const WebsitesSelectorsManagement = () => {
   // const state = useSelector(state => state);
   const dispatch = useDispatch()
 
   const [websitesSelectors, setWebsitesSelectors] = useState([]);
+  const [isTesting, setIsTesting] = useState(false);
   const [websitesSelectorsUnchanged, setWebsitesSelectorsUnchanged] = useState([]);
-  const [testProductsData, setTestProductsData] = useState([]);
+  const [testData, setTestData] = useState(testDataaa)
+  console.log('testData: ', testData);
 
   useEffect(() => {
     dispatch(getWebsitesSelectors())
@@ -54,7 +50,17 @@ const WebsitesSelectorsManagement = () => {
         .catch(() => {
           alert(`${singleRowData.id} hasn't been updated`)
         })
-  )
+    )
+  }
+
+  const testProducts = (rowData) => {
+      setIsTesting(true)
+      return dispatch(testProductsScan(rowData))
+          .then(res => {
+              setTestData(res);
+              setIsTesting(false)
+          })
+          .catch(console.error)
   }
 
    const onRowUpdate = (newData) => {
@@ -63,9 +69,7 @@ const WebsitesSelectorsManagement = () => {
         return Promise.resolve();
   };
 
-  const testScan = (rowData) => {
-    setTestProductsData(rowData)
-  }
+  const onModalClose = () => setTestData([]);
 
   const actions = [
     {
@@ -76,7 +80,7 @@ const WebsitesSelectorsManagement = () => {
     {
       icon: 'T',
       tooltip: 'Test selectors',
-      onClick: (event, rowData) => testScan(rowData)
+      onClick: (event, rowData) => testProducts(rowData)
     }
   ]
 
@@ -84,13 +88,14 @@ const WebsitesSelectorsManagement = () => {
       <div className="website-products-management">
         <Modal
           className="website-products-management__modal-product-test"
-          open={testProductsData.length}
+          open={testData.length}
         >
           <Card>
-              <ProductTestScan testProductsData={testProductsData} onRowUpdate={onRowUpdate}/>
+              <ProductTestedData testData={testData} onModalClose={onModalClose}/>
           </Card>
         </Modal>
         <MaterialTable
+          isLoading={isTesting}
           icons={tableIcons}
           columns={columns}
           data={websitesSelectors}
