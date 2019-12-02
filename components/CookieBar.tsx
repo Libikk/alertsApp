368c68d23bcaaa13194911e96bc4491e45974aea
Libikk <libikpl@gmail.com>
2019-12-02 06:52:07 +0000

    cookie bar and local storage service

--- components/CookieBar.tsx
+++ components/CookieBar.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import Link from 'next/link';
 import Button from '@material-ui/core/Button';
+import { LocalStorageService } from '../utils/LocalStorageService';
 import CookieIcon from '../static/svg/cookie.svg';
 import '../styles/cookieBar.scss';
 
 const CookieBar = () => {
+    const [displayCookie, setDisplayCookie] = useState(false);
 
+    const agreeCookies = () => {
+        LocalStorageService.setItem('cookie-agreed', true)
+        setDisplayCookie(false);
+    }
 
-    return <div className="cookie-container">
+    useEffect(() => {
+        if (!LocalStorageService.getItem('cookie-agreed')) {
+            setDisplayCookie(true)
+        } else {
+            setDisplayCookie(false)
+        }
+      });
+
+    return displayCookie && <div className="cookie-container">
         <div className="cookie-container__head">
-            <CookieIcon /> This website is using cookie to enhance the user experience. <Link href={{ pathname: 'privacyPolicy' }} as='/privacypolicy'><a>Privacy Policy</a></Link>
+            <CookieIcon /> This website is using cookie to enhance the user experience. <Link href='/privacypolicy'><a>Privacy Policy</a></Link>
         </div>
         <div className="cookie-container__body">
-         <Button variant="contained" color="secondary">Agree</Button>
+         <Button onClick={agreeCookies} variant="contained" color="secondary">Agree</Button>
         </div>
     </div>
 }
