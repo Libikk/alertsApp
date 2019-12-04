b3e24032a0f3a47a33b9b258e1d26fbd82ab6571
Libikk <libikpl@gmail.com>
2019-12-04 06:49:30 +0000

    add transition to cxookie bar

--- components/CookieBar.tsx
+++ components/CookieBar.tsx
@@ -3,10 +3,17 @@ import Link from 'next/link';
 import Button from '@material-ui/core/Button';
 import { LocalStorageService } from '../utils/LocalStorageService';
 import CookieIcon from '../static/svg/cookie.svg';
+import { useSpring, animated } from 'react-spring'
 import '../styles/cookieBar.scss';
 
 const CookieBar = () => {
     const [displayCookie, setDisplayCookie] = useState(false);
+    const props = useSpring({
+        transform: 'translate3d(0,0,0)',
+        from: {
+            transform: 'translate3d(400px,0,0)'
+            }
+        })
 
     const agreeCookies = () => {
         LocalStorageService.setItem('cookie-agreed', true)
@@ -21,14 +28,14 @@ const CookieBar = () => {
         }
       });
 
-    return displayCookie && <div className="cookie-container">
+    return displayCookie && <animated.div className="cookie-container" style={props}>
         <div className="cookie-container__head">
             <CookieIcon /> This website is using cookie to enhance the user experience. <Link href='/privacypolicy'><a>Privacy Policy</a></Link>
         </div>
         <div className="cookie-container__body">
          <Button onClick={agreeCookies} variant="contained" color="secondary">Agree</Button>
         </div>
-    </div>
+    </animated.div>
 }
 
 export default CookieBar;
