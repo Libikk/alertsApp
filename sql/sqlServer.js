24fe2388a7add9a6f860ba574962b97e955ab8f7
Libikk <libikpl@gmail.com>
2019-12-14 17:21:54 +0000

    add debug mode to the sql server

--- sql/sqlServer.js
+++ sql/sqlServer.js
@@ -6,23 +6,27 @@ const sqlCredential = require('./sqlCredential');
 const sql = require('mysql').createConnection(Object.assign({}, sqlCredential[env], { multipleStatements: true }));
 
 const mysqlQueries = {
-  executeRawSQL: (query, params) => new Promise((resolve, reject) => {
+  executeRawSQL: (query, params, isDebug) => new Promise((resolve, reject) => {
+    if (isDebug) {
+      console.log('query: ', query);
+      console.log('params: ', params);
+    }
     sql.query(query, params, (error, response) => {
       if (error) reject(error);
       resolve(response);
     });
   }),
-  sqlQuery: (queryNameOrRaw, params) => {
+  sqlQuery: (queryNameOrRaw, params, isDebug) => {
     const rawSql = getQuery(queryNameOrRaw) || queryNameOrRaw;
-    if (_.isObject(params) && !_.isArray(params)) return mysqlQueries.replaceVariablesInQuery(params, rawSql);
-    return mysqlQueries.executeRawSQL(rawSql, params);
+    if (_.isObject(params) && !_.isArray(params)) return mysqlQueries.replaceVariablesInQuery(params, rawSql, isDebug);
+    return mysqlQueries.executeRawSQL(rawSql, params, isDebug);
   },
-  replaceVariablesInQuery: (params, rawSql) => {
+  replaceVariablesInQuery: (params, rawSql, isDebug) => {
     const replacedParamsSql = Object.keys(params).reduce((query, nextParamKey) => {
       const singleParamValue = sql.escape(params[nextParamKey]);
       return query.replace(new RegExp(nextParamKey, 'g'), singleParamValue);
     }, rawSql);
-    return mysqlQueries.executeRawSQL(replacedParamsSql);
+    return mysqlQueries.executeRawSQL(replacedParamsSql, null, isDebug);
   },
   mapKeysToParams: object => Object.keys(object).reduce((acc, nextObj) => {
     acc[`@${nextObj}`] = object[nextObj];
