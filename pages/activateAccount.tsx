0c3e5bc05b33a35a1f070b4c802f3600175d6b06
Libikk <libikpl@gmail.com>
2020-01-05 08:22:53 +0000

    refactor: convert class to react hooks !

--- pages/activateAccount.tsx
+++ pages/activateAccount.tsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import Layout from '../components/Layout';
 import defaultPage from '../components/Auth/defaultPage';
-import { connect } from 'react-redux';
 
 type MyProps = {
-  isActivated: boolean,
-  isAlreadyActive: number,
-  messageData: {
-    msg: string,
-    isError: boolean
+  result: {
+    isActivated: boolean,
+    isAlreadyActive: number,
+    messageData: {
+      msg: string,
+      isError: boolean
+    }
   }
 };
 
-class ActivateAccount extends React.Component<MyProps>  {
-  render () {
-    return (
-          <Layout>
-              <div>
-                  <h1>{this.props.messageData.msg}</h1>
-              </div>
-          </Layout>
-    )
-  }
-}
-const mapDispatchToProps = dispatch => ({
+const ActivateAccount = (props: MyProps) => (
+  <Layout>
+      <div>
+          <h1>{props.result.messageData.msg}</h1>
+      </div>
+  </Layout>
+)
 
-});
+ActivateAccount.getInitialProps = async({ query }) => ({ ...query });
 
-export default connect(state => state, mapDispatchToProps)(defaultPage(ActivateAccount));
+export default defaultPage(ActivateAccount);
