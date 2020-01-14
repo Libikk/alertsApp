b56ea0e75ea1c61b2ab6dda706d81e9af11724b7
Libikk <libikpl@gmail.com>
2020-01-14 20:07:25 +0000

    fix the nav

--- components/Shared/PagesTabs.tsx
+++ components/Shared/PagesTabs.tsx
@@ -5,10 +5,9 @@ import Router from 'next/router';
 
 const PagesTabs = (pageIndex) => {
     const handleChange = (event, newValue) => {
-      console.log('newValue: ', newValue);
         if (newValue === 0) Router.push('/about', 'about');
         else if (newValue === 1) Router.push('/termsAndConditions', 'termsandconditions');
-        else if (newValue === 2) Router.push('/privacyPolicy', 'privacypolicy');
+        else if (newValue === 2) Router.push('/privacypolicy', 'privacypolicy');
     };
 
     return <div>
