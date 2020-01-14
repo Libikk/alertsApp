4718fe9b7052d0b304bcee0082c0ef248b2da534
Libikk <libikpl@gmail.com>
2020-01-14 20:10:13 +0000

    fix: change page case

--- components/Shared/PagesTabs.tsx
+++ components/Shared/PagesTabs.tsx
@@ -6,7 +6,7 @@ import Router from 'next/router';
 const PagesTabs = (pageIndex) => {
     const handleChange = (event, newValue) => {
         if (newValue === 0) Router.push('/about', 'about');
-        else if (newValue === 1) Router.push('/termsAndConditions', 'termsandconditions');
+        else if (newValue === 1) Router.push('/termsandconditions', 'termsandconditions');
         else if (newValue === 2) Router.push('/privacypolicy', 'privacypolicy');
     };
 
