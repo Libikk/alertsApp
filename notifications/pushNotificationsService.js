1e824e928dbcdb281c650e8fe62bd3eb2574d86e
Libikk <libikpl@gmail.com>
2020-01-27 07:29:05 +0000

    chore: expo logging errors

--- notifications/pushNotificationsService.js
+++ notifications/pushNotificationsService.js
@@ -26,14 +26,15 @@ const pushNotifications = {
 
     return pushNotifications.send(structuredPushNotifications);
   },
-  send: (messages) => {
+  send: async (messages) => {
     /* eslint-disable */
     // /////////////
     // // TO DO ////
     // /////////////
+    // THIS IF FUCKING CRAP, GOD BLESS AUTHOR...
+    
     const chunks = expo.chunkPushNotifications(messages);
     const tickets = [];
-    (async () => {
       // Send the chunks to the Expo push notification service. There are
       // different strategies you could use. A simple one is to send one chunk at a
       // time, which nicely spreads the load out over time:
@@ -54,70 +55,31 @@ const pushNotifications = {
           });
         }
       }
-    })();
-    console.log('chunks: ', chunks);
 
+    const receiptIds = tickets.reduce((acc, { id }) => acc.concat(id || []), []);
 
-    // Later, after the Expo push notification service has delivered the
-    // notifications to Apple or Google (usually quickly, but allow the the service
-    // up to 30 minutes when under load), a "receipt" for each notification is
-    // created. The receipts will be available for at least a day; stale receipts
-    // are deleted.
-    //
-    // The ID of each receipt is sent back in the response "ticket" for each
-    // notification. In summary, sending a notification produces a ticket, which
-    // contains a receipt ID you later use to get the receipt.
-    //
-    // The receipts may contain error codes to which you must respond. In
-    // particular, Apple or Google may block apps that continue to send
-    // notifications to devices that have blocked notifications or have uninstalled
-    // your app. Expo does not control this policy and sends back the feedback from
-    // Apple and Google so you can handle it appropriately.
-    const receiptIds = [];
-    for (const ticket of tickets) {
-      // NOTE: Not all tickets have IDs; for example, tickets for notifications
-      // that could not be enqueued will have error information and no receipt ID.
-      if (ticket.id) {
-        receiptIds.push(ticket.id);
-      }
-    }
-
-    const receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-
-    (async () => {
+    const receiptIdChunks = await expo.chunkPushNotificationReceiptIds(receiptIds);
       // Like sending notifications, there are different strategies you could use
       // to retrieve batches of receipts from the Expo service.
       for (const chunk of receiptIdChunks) {
-        try {
-          const receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-
-          // The receipts specify whether Apple or Google successfully received the
-          // notification and information about an error, if one occurred.
-          for (const receipt of receipts) {
-            if (receipt.status === 'ok') {
-              continue;
-            } else if (receipt.status === 'error') {
-              console.error(`There was an error sending a notification: ${receipt.message}`);
-
-              Sentry.withScope((scope) => {
-                scope.setExtra('data', { receipt });
-                Sentry.captureException(error);
-              });
-
-              if (receipt.details && receipt.details.error) {
-                // The error codes are listed in the Expo documentation:
-                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
-                // You must handle the errors appropriately.
-                console.error(`The error code is ${receipt.details.error}`);
-              }
-            }
+          const receipts = await expo.getPushNotificationReceiptsAsync(chunk)
+          .catch(err => {
+            Sentry.withScope((scope) => {
+              scope.setExtra('data', { chunk });
+              Sentry.captureException(err);
+            });
+          });
+         
+          
+          if (receipts.status !== 'ok') {
+            Sentry.withScope((scope) => {
+              scope.setExtra('data', { receipts, tickets, chunk });
+              Sentry.captureException(new Error('Receipt error'));
+            });
+          } else {
+            console.log('Notification has been sent sucessfully: ', receipts);
           }
-        } catch (error) {
-          console.error(error);
-        }
       }
-    })();
-    /* eslint-enable */
   },
 };
 
