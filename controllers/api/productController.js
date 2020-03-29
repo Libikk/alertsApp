ac494484cbefbfab08bd56bf53c6af6f64d28e6d
Libikk <libikpl@gmail.com>
2020-03-29 09:22:28 +0100

    chore:remove api to to check if product exist

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -8,36 +8,27 @@ const _ = require('lodash');
 const router = express.Router();
 const { sqlQuery } = require('../../sql/sqlServer');
 
-const checkProdExistence = (req, res, next) => {
-  sqlQuery('checkProductExistence', [req.query.productUrlData])
-    .then(response => res.send(response.pop()))
-    .catch(next);
-};
-
-
 const addUserProduct = async (req, res, next) => {
-  // eslint-disable-next-line prefer-const
   const { productUrl } = req.body;
   let productId = await sqlQuery('checkProductExistence', [productUrl]).then(response => _.get(response, '0.productId', null)).catch(next);
-  console.log('productId: ', productId, typeof productId);
 
-  // if (!productId) {
-  //   const parsedUrl = url.parse(productUrl);
+  if (!productId) {
+    const parsedUrl = url.parse(productUrl);
 
-  //   const hostName = parsedUrl.host.replace(/(?:www\.)?/i, '');
-  //   await sqlQuery('createNewWebsite', { '@host': hostName, '@hostUrl': `${parsedUrl.protocol}//${parsedUrl.host}` }).catch(next);
+    const hostName = parsedUrl.host.replace(/(?:www\.)?/i, '');
+    await sqlQuery('createNewWebsite', { '@host': hostName, '@hostUrl': `${parsedUrl.protocol}//${parsedUrl.host}` }).catch(next);
 
-  //   const newProduct = await sqlQuery('createNewProduct', { '@productUrl': parsedUrl.href, '@hostName': hostName }).catch(next);
-  //   if (!newProduct.affectedRows) {
-  //     return next(Object.assign(new Error(), { name: 'PRODUCT_ALREADY_EXIST' }));
-  //   }
+    const newProduct = await sqlQuery('createNewProduct', { '@productUrl': parsedUrl.href, '@hostName': hostName }).catch(next);
+    if (!newProduct.affectedRows) {
+      return next(Object.assign(new Error(), { name: 'PRODUCT_ALREADY_EXIST' }));
+    }
 
-  //   productId = newProduct.insertId;
-  // }
+    productId = newProduct.insertId;
+  }
 
-  // const userProduct = await sqlQuery('addUserProduct', { '@productId': productId, '@userId': req.user.userId }).catch(next);
+  const userProduct = await sqlQuery('addUserProduct', { '@productId': productId, '@userId': req.user.userId }).catch(next);
 
-  // res.send({ productId: userProduct.insertId });
+  res.send({ productId: userProduct.insertId });
 };
 
 const getUserProducts = async (req, res, next) => {
@@ -50,7 +41,6 @@ const deleteUserProduct = async (req, res, next) => {
   res.send(deletedProduct);
 };
 
-router.get('/productExistence', passport.authenticate('jwt', { session: false }), validate(schema['GET:/api/product/productExistence']), throwInvalid, checkProdExistence);
 router.post('/addUserProduct', passport.authenticate('jwt', { session: false }), validate(schema['POST:/api/product/addUserProduct']), throwInvalid, addUserProduct);
 router.get('/getUserProducts', passport.authenticate('jwt', { session: false }), getUserProducts);
 router.delete('/deleteUserProduct/:productId', passport.authenticate('jwt', { session: false }), validate(schema['POST:/api/product/deleteUserProduct']), throwInvalid, deleteUserProduct);
--- dispatchers/productDispatchers.ts
+++ dispatchers/productDispatchers.ts
@@ -1,11 +1,4 @@
-// const { sqlQuery } = require('../sql/sqlServer');
 import { ProductService } from '../api/productService';
-
-
-export const checkProdExistence = (productUrlData) => (dispatch) => {
-  return ProductService.checkProdExistence(productUrlData)
-    .then(res => dispatch({ type: 'CHECK_PRODUCT_EXISTENCE', prodData: res })
-)};
 
 export const addUserProduct = (productUrlData) => (dispatch) => {
   return ProductService.addUserProduct(productUrlData)
