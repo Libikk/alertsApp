8034f40b5899b171ba89268580c94ff9c1ca04fe
Libikk <libikpl@gmail.com>
2020-04-04 13:59:06 +0100

    chore: fix case issue - linux case sensitive

--- controllers/sqlQueries/getProductsForManagement.sql
+++ controllers/sqlQueries/getProductsForManagement.sql
@@ -5,7 +5,7 @@ SELECT
     web.url AS "websiteUrl"
 FROM   discounthero.products prod 
 JOIN   discounthero.websites web ON prod.websiteid = web.websiteid 
-JOIN   discounthero.websitesselectors webSelectors ON webselectors.websiteid = web.websiteid 
+JOIN   discounthero.websitesSelectors webSelectors ON webselectors.websiteid = web.websiteid 
 WHERE  web.isActive = 1
 AND    prod.isActive = 0
 AND STR_TO_DATE(prod.deletedAt, '%d-%b-%Y') IS NULL
