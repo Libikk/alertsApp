d328681060cab515979f32b08c47e7751d8d2335
Libikk <libikpl@gmail.com>
2020-04-05 16:23:25 +0100

    chore: change query to take from specific if these dont exist then take standard ones

--- controllers/sqlQueries/getDataForScan.sql
+++ controllers/sqlQueries/getDataForScan.sql
@@ -1,17 +1,19 @@
 SELECT
-    productId,
+    prod.productId,
     web.websiteId,
     prod.productUrl as "fullUrl",
-	webSelectors.isDiscountSelectorRegex AS "regex",
-	webSelectors.isDiscountSelector AS "selectorString",
-	webSelectors.isClientSideCheck,
-    webSelectors.imageSelector,
-	webSelectors.productNameSelector,
-	webSelectors.productPriceSelector,
-	webSelectors.productDiscountedPriceSelector
+    CASE WHEN ps.isDiscountSelector IS NOT NULL THEN ps.isDiscountSelector ELSE ws.isDiscountSelector END AS "selectorString",
+    CASE WHEN ps.isDiscountSelectorRegex IS NOT NULL THEN ps.isDiscountSelectorRegex ELSE ws.isDiscountSelectorRegex END AS "isDiscountSelectorRegex",
+    CASE WHEN ps.isClientSideCheck IS NOT NULL THEN ps.isClientSideCheck ELSE ws.isClientSideCheck END AS "isClientSideCheck",
+	CASE WHEN ps.imageSelector IS NOT NULL THEN ps.imageSelector ELSE ws.imageSelector END AS "imageSelector",
+    CASE WHEN ps.productNameSelector IS NOT NULL THEN ps.productNameSelector ELSE ws.productNameSelector END AS "productNameSelector",
+	CASE WHEN ps.productPriceSelector IS NOT NULL THEN ps.productPriceSelector ELSE ws.productPriceSelector END AS "productPriceSelector",
+    CASE WHEN ps.productDiscountedPriceSelector IS NOT NULL THEN ps.productDiscountedPriceSelector ELSE ws.productDiscountedPriceSelector END AS "productDiscountedPriceSelector"
 FROM discounthero.products prod
 JOIN discounthero.websites web on prod.websiteId = web.websiteId
-JOIN discounthero.websitesSelectors webSelectors ON webSelectors.websiteId = web.websiteId
+JOIN discounthero.websitesSelectors ws ON ws.websiteId = web.websiteId
+LEFT JOIN discounthero.productsselectors ps ON ps.productId = prod.productId 
 WHERE web.isActive = 1
     AND prod.isActive = 1
     AND (CASE WHEN @productId IS NULL THEN true ELSE prod.productId = @productId END)
+    and prod.productId = 81
