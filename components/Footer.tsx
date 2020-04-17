aa0e9cd617954b27302fe089be451e5855ddc307
Libikk <libikpl@gmail.com>
2020-04-17 07:31:45 +0100

    style: add badge and styles for reponsivness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,14 @@ export default class Footer extends React.Component {
       return (
         <div className="container__footer">
             <nav className="footer_nav">
+              <div className="nav__android-container">
+                <a href='https://play.google.com/store/apps/details?id=ddiscount.hero&hl=en_GB&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                  <img alt='Get it on Google Play' src='https://play.google.com/intl/en_gb/badges/static/images/badges/en_badge_web_generic.png'/>
+                </a>
+              </div>
                 <ul className="nav__link-list">
+                   
+                   
                     {navConfig.map(nav => <li key={nav.displayName}>
                       <Link href={nav.url}>
                         <a>
