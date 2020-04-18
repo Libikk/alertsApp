a8a574c70ba24249e7e04a34a86fc905dd8b08ec
Libikk <libikpl@gmail.com>
2020-04-18 12:14:04 +0100

    change about page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,17 +10,15 @@ const About = () => {
             <Paper className="about-container__content">
                 <h1>About DDiscount hero </h1>
 
-                <p>As we all know there are lots of products which get discounted for a specific period of time and then after that it get back to normal :(
-                    I think no one like to pay more for something that could get a cheaper, waiting for discount sometimes cost too much attention.</p>
+                <p>As we all know there are lots of products which get discounted for a specific period of time and then after that it gets back to normal :(
+                    I think no one likes to pay more for something they could get cheaper. DDiscount hero is here to help.</p>
 
-                <p>As I respect the time I don't want to spend it on this sort of things, there is plenty of the amazing stuff that I want to do... and build :)</p>
+                <p>As I respect time, I don't want to spend it on continuous discount hunting, as there is plenty of other amazing things I want to do... and create :)</p>
 
-                <p>I thought to myself... there must be a way to make this process automated! couple of days later, a very rough, blurry solution came up in my head.
-                    I saw the "whole thing", which was later named as DDiscount hero, pretty much functional. Programming experience and working with the latest technology,
-                    helped me to design the system which automates the entire process!</p>
+                <p>I thought to myself - there must be a way around it! Couple of days later, a very rough, blurry solution came up in my head. I saw the "whole thing", which was later named DDiscount Hero, easy and functional site to use. Programming experience and working with the latest technology, helped me to design the system which allows anybody to find the product they love, at a cheaper price!</p>
 
 
-                <p>Marek Kregiel - <a href="https://www.linkedin.com/in/marek-kregiel-259925103" target="_blank">LinkedIn</a></p>
+                <p>Marek Kregiel - <a href="https://www.linkedin.com/in/marek-kregiel-259925103" target="_blank"><b>LinkedIn</b></a> or <b>developer@marekregiel.com</b></p>
             </Paper>
         </div>
     </Layout>
